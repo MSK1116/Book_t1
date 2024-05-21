6a917bd9c080ca4068937a5dc8e7278e4db91d76
MSK1116 <anishkushwaha1116@gmail.com>
2024-05-21 19:54:35 +0545

    push

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,16 +2,7 @@ import React from "react";
 import Logo from "../../public/logo.png";
 
 const Header = () => {
-  return (
-    <>
-      <head>
-        <meta charset="UTF-8" />
-        <link rel="icon" type="image/png" href="/logo.png" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>ETN</title>
-      </head>
-    </>
-  );
+  return <></>;
 };
 
 export default Header;
